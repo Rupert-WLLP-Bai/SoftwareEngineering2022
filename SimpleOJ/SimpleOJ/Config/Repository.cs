a6dba08c3fdd2971fa9446906603c98e507dc2d7
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2022-11-06 23:18:40 +0800

    仓储类 具体连接配置可以改为Config中用类读取json

---
+++ SimpleOJ/SimpleOJ/Config/Repository.cs
@@ -0,0 +1,33 @@
+﻿using SqlSugar;
+
+namespace SimpleOJ.Config {
+    public class Repository<T> : SimpleClient<T> where T : class, new() {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="context"></param>
+        public Repository(ISqlSugarClient? context = null) : base(context) {
+            if (context == null) {
+                base.Context = Db;
+            }
+        }
+
+        /// <summary>
+        /// SqlSugarScope操作数据库是线程安全的可以单例
+        /// </summary>
+        public static readonly SqlSugarScope Db = new SqlSugarScope(
+            new ConnectionConfig()
+            {
+                DbType = SqlSugar.DbType.MySql,
+                ConnectionString = @"server=119.3.154.46;Database=SE2022;Uid=bjh;Pwd=1230;",
+                IsAutoCloseConnection = true
+            },
+            db => {
+                // 输出日志
+                db.Aop.OnLogExecuting = (s, p) => {
+                    Console.WriteLine(s);
+                    Console.WriteLine(string.Join(",", p.Select(a => a.ParameterName + ":" + a.Value)));
+                };
+            });
+    }
+}
