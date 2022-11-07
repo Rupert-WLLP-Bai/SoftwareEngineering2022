44bdd2e20ded3dafe6274cd4ccfd2c9f748f498a
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2022-11-07 15:44:59 +0800

    20221107 更新了密码加密

---
+++ SimpleOJ/SimpleOJ/Util/EncryptPassword.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleOJ.Util {
+    /// <summary>
+    /// 加密密码
+    /// </summary>
+    public static class EncryptPassword {
+        // 使用SHA256加密密码
+        private static readonly SHA256 Sha256 = SHA256.Create();
+
+        public static string Encrypt(string password, string salt) {
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var saltBytes = Encoding.UTF8.GetBytes(salt);
+            var passwordAndSaltBytes = passwordBytes.Concat(saltBytes).ToArray();
+            var hashBytes = Sha256.ComputeHash(passwordAndSaltBytes);
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
+ 
