9a3bbd3ec05abe2e728155651bcde5d5616465f2
theTJCloudWalker <81422226+theTJCloudWalker@users.noreply.github.com>
2022-11-08 17:36:24 +0800

    Add files via upload

---
+++ SimpleOJ/SimpleOJ/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleOJ.Common;
+using SimpleOJ.Service;
+
+namespace SimpleOJ.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly IJwtTokenService _jwtTokenService;
+
+        public TestController()
+        {
+            _jwtTokenService = new JwtTokenService();
+        }
+
+        [HttpPost("GenToken")]
+        public Result GenToken([FromBody] UserToken userInfo) {
+            return new Result(ResultCode.Success,_jwtTokenService.GenerateToken(userInfo,DateTime.Now));
+        }
+        
+        [HttpPost("DecodeToken")]
+        public Result DecodeToken([FromBody] string token) {
+            return new Result(ResultCode.Success,_jwtTokenService.DeocdeJwtToken(token,null));
+        }
+        
+        [HttpPost("UpdateToken")]
+        public Result Update(UserToken userInfo) {
+            return new Result(ResultCode.Success,_jwtTokenService.UpdateToken(userInfo));
+        }
+        
+        [HttpPost("UpdateToken2")]
+        public Result UpdateToken(string token) {
+            return new Result(ResultCode.Success,_jwtTokenService.UpdateToken(token));
+        }
+        
+        [HttpPost("VerifyToken")]
+        public Result VerifyToken(string token) {
+            return new Result(ResultCode.Success,_jwtTokenService.VerifyToken(token,null));
+        }
+
+        
+        
+    }
+}
