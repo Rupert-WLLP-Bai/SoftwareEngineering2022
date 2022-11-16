f5658f7c8b65506e3ed2c96c545f626c2aa59818
theTJCloudWalker <81422226+theTJCloudWalker@users.noreply.github.com>
2022-11-16 23:04:28 +0800

    Add files via upload

---
+++ SimpleOJ/SimpleOJ/Controllers/FileController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimpleOJ.Common;
+using SimpleOJ.Util;
+
+namespace SimpleOJ.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileController:IFileController
+    {
+        /*public FileResult Download(string fileId)
+        {
+            
+        }*/
+        [Authorize(Roles = "Admin")]
+        [HttpPost("Upload")]
+        [RequestSizeLimit(1024*1024*1024)]
+        public dynamic Upload(IEnumerable<IFormFile> files,string userId)
+        {
+            List<string> f = new List<string>();
+            foreach (var file in files)
+            {
+                var fileName = file.FileName;
+                f.Add(fileName);
+                //获取文件名后缀
+                string fileExtension = file.FileName.Substring(file.FileName.LastIndexOf(".") + 1);
+                //保存文件
+                var stream = file.OpenReadStream();
+                //把stream转换成byte[]
+                byte[] bytes = new byte[stream.Length];
+                _ = stream.Read(bytes,0,bytes.Length);
+                //设置当前流的位置为流的开始
+                stream.Seek(0,SeekOrigin.Begin);
+                //把byte[]写入文件
+                FileStream fs = new FileStream(PathSetting.Instance.UserDirectory+"/debug/"+file.FileName,FileMode.Create);
+                BinaryWriter bw = new BinaryWriter(fs);
+                bw.Write(bytes);
+                bw.Close();
+                fs.Close();
+            }
+            
+            return new OldResult(OldResultCode.Success, f);
+        }
+    }
+}
---
+++ SimpleOJ/SimpleOJ/Controllers/IFileController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleOJ.Controllers
+{
+
+    public interface IFileController
+    {
+        //public FileResult Download(string fileId);
+
+        public dynamic Upload(IEnumerable<IFormFile> files,string userId);
+    }
+}
