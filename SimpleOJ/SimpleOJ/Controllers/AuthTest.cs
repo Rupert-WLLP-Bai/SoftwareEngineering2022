23c6055feba63c4e1de3125ab7cf2c39cfa6726c
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2022-11-23 20:11:48 +0800

    update authtest 1. update token

--- SimpleOJ/SimpleOJ/Controllers/AuthTest.cs
+++ SimpleOJ/SimpleOJ/Controllers/AuthTest.cs
@@ -2,18 +2,28 @@ using log4net;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleOJ.Common;
+using SimpleOJ.Model;
+using SimpleOJ.Service;
 
 namespace SimpleOJ.Controllers {
     [ApiController]
     [Route("api/[controller]")]
     public class AuthTest {
         private static readonly ILog Log = LogManager.GetLogger(typeof(AuthTest));
+        private readonly IJwtTokenService _jwtTokenService;
+        private AuthTest() {
+            _jwtTokenService = new JwtTokenService();
+        }
+        public record AuthTestResult(string Message, string Token);
         [TestMethod]
         [HttpGet("test")]
         [Authorize(Roles = "Admin")]
-        public string Auth() {
+        public Result<AuthTestResult> Auth() {
             Log.Info("通过Authorize验证");
-            return "authorization success!";
+            var updateToken = _jwtTokenService.UpdateToken(new UserToken("admin",User.UserRole.Admin));
+            return new Result<AuthTestResult>(true, ResultCode.Success, 
+                new AuthTestResult("authorization success!", updateToken));
         }
     }
 }
