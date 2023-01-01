957bf00e9e01a09a92cb4de804dab636ab6b1695
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-01-01 13:58:14 +0800

    2023 1/1

--- SimpleOJ/SimpleOJ/Controllers/FileController.cs
+++ SimpleOJ/SimpleOJ/Controllers/FileController.cs
@@ -3,44 +3,40 @@ using Microsoft.AspNetCore.Mvc;
 using SimpleOJ.Common;
 using SimpleOJ.Util;
 
-namespace SimpleOJ.Controllers
-{
+namespace SimpleOJ.Controllers {
     [ApiController]
     [Route("api/[controller]")]
-    public class FileController:ControllerBase
-    {
+    public class FileController : IFileController {
         /*public FileResult Download(string fileId)
         {
             
         }*/
-        // [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         [HttpPost("Upload")]
-        [RequestSizeLimit(1024*1024*1024)]
-        public dynamic Upload(IEnumerable<IFormFile> files,string userId)
-        {
-            List<string> f = new List<string>();
-            foreach (var file in files)
-            {
+        [RequestSizeLimit(1024 * 1024 * 1024)]
+        public Result<IEnumerable<string>> Upload(IEnumerable<IFormFile> files, string userId) {
+            var f = new List<string>();
+            foreach (var file in files) {
                 var fileName = file.FileName;
                 f.Add(fileName);
                 //获取文件名后缀
-                string fileExtension = file.FileName.Substring(file.FileName.LastIndexOf(".") + 1);
+                var fileExtension = file.FileName.Substring(file.FileName.LastIndexOf(".", StringComparison.Ordinal) + 1);
                 //保存文件
                 var stream = file.OpenReadStream();
                 //把stream转换成byte[]
-                byte[] bytes = new byte[stream.Length];
-                _ = stream.Read(bytes,0,bytes.Length);
+                var bytes = new byte[stream.Length];
+                _ = stream.Read(bytes, 0, bytes.Length);
                 //设置当前流的位置为流的开始
-                stream.Seek(0,SeekOrigin.Begin);
+                stream.Seek(0, SeekOrigin.Begin);
                 //把byte[]写入文件
-                FileStream fs = new FileStream(PathSetting.Instance.UserDirectory+"/debug/"+file.FileName,FileMode.Create);
-                BinaryWriter bw = new BinaryWriter(fs);
+                var fs = new FileStream(PathSetting.Instance.UserDirectory + "/debug/" + file.FileName, FileMode.Create);
+                var bw = new BinaryWriter(fs);
                 bw.Write(bytes);
                 bw.Close();
                 fs.Close();
             }
 
-            return new Result<List<string>>(true, ResultCode.Success, f);
+            return new Result<IEnumerable<string>>(true,ResultCode.Success, f);
         }
     }
 }
